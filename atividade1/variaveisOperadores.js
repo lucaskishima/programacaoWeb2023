af1586d27d3500ecbc5710c3f4ed63d0b6c45f32
lucas kishima <lucas@debian.lucas.br>
2023-10-30 14:20:31 -0300

    adicionando atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,14 @@
+var numA = 5;
+var numB = 3;
+
+var resultadoSoma = numA + numB;
+console.log(`Resultado da adição entre ${numA} e ${numB} = ${resultadoSoma}`);
+
+var resultadoSubtracao = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB} = ${resultadoSubtracao}`);
+
+var resultadoMultiplicacao = numA * numB;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB} = ${resultadoMultiplicacao}`);
+
+var resultadoDivisao = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB} = ${resultadoDivisao}`);
